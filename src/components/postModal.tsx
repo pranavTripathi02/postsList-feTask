648d1597bf73b2d103ee9092dc660aba3907be75
Pranav Tripathi <pranav02.tripathi@gmail.com>
2024-06-15 16:31:38 +0530

    add post modal

---
+++ src/components/postModal.tsx
@@ -0,0 +1,70 @@
+import { TPost } from "../types";
+import { forwardRef } from "react";
+
+type TProps = {
+  post: TPost;
+  toggleModal: () => void;
+};
+
+const PostModal = forwardRef<HTMLDialogElement, TProps>((props, ref) => {
+  const { post, toggleModal } = props;
+
+  return (
+    <dialog
+      ref={ref}
+      className="modal"
+    >
+      <header className="modal-header">
+        <button
+          className="modal-closeBtn"
+          onClick={toggleModal}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="lucide lucide-x"
+          >
+            <path d="M18 6 6 18" />
+            <path d="m6 6 12 12" />
+          </svg>
+        </button>
+      </header>
+      <div className="modal-img-container">
+        <img
+          src={post.thumbnailLarge}
+          alt={`Image for ${post.title}`}
+          className="modal-img"
+        />
+      </div>
+      <div className="modal-title">{post.title}</div>
+      <div className="modal-desc">{post.content}</div>
+      <div className="modal-footer">
+        <div className="author-avatar">
+          <img
+            src={post.authorAvatar}
+            alt={`User ${post.authorName} image`}
+            className="author-avatar-img"
+          />
+        </div>
+        <div>
+          <span>{post.authorName}</span>
+          {!!post.authorRole && (
+            <>
+              <span>-</span>
+              <span>{post.authorRole}</span>
+            </>
+          )}
+        </div>
+      </div>
+    </dialog>
+  );
+});
+
+export default PostModal;
