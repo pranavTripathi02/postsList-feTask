05dd8770acede016026ce84996e8276643be388a
Pranav Tripathi <pranav02.tripathi@gmail.com>
2024-06-16 05:01:57 +0530

    add loading state to home

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,31 @@ import { useEffect, useState, useRef } from "react";
 import getPosts from "./api/posts/posts";
 import { TPost } from "./types/index.ts";
 import PostCard from "./components/postCard";
+import Loading from "./components/loading/loading";
 import PostModal from "./components/postModal";
 
 function App() {
   const [posts, setPosts] = useState<TPost[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
   const [selectedPost, setSelectedPost] = useState<TPost | null>(null);
   const modalRef = useRef<HTMLDialogElement>(null);
+
+  const fetchPosts = async () => {
+    setIsLoading(true);
+    try {
+      const fetchedPosts = await getPosts();
+      setIsLoading(false);
+      setPosts(fetchedPosts);
+    } catch (err) {
+      console.error(err);
+      setIsLoading(false);
+      throw new Error("Unable to fetch posts.");
+    }
+  };
+
   useEffect(() => {
-    getPosts().then((res) => setPosts(res));
+    // getPosts().then((res) => setPosts(res));
+    fetchPosts();
   }, []);
 
   const toggleModal = () => {
@@ -23,7 +40,9 @@ function App() {
       : modalRef.current.showModal();
   };
 
-  return (
+  return isLoading ? (
+    <Loading />
+  ) : (
     <>
       {!!selectedPost && (
         <PostModal
---
+++ src/components/loading/loading.tsx
@@ -0,0 +1,28 @@
+import "./loading.scss";
+
+function Loading() {
+  return (
+    <div className="loading-container">
+      <span className="spinner">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="64"
+          height="64"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="lucide lucide-loader-circle"
+        >
+          <path d="M21 12a9 9 0 1 1-6.219-8.56" />
+        </svg>
+      </span>
+      <span>Loading Posts...</span>
+      <span>Thank you for waiting.</span>
+    </div>
+  );
+}
+
+export default Loading;
