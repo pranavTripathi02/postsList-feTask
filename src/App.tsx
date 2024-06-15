1fd973ccca3596d423bad526cdf21f0390629330
Pranav Tripathi <pranav02.tripathi@gmail.com>
2024-06-15 17:13:46 +0530

    integrate modal into app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
 import "./App.scss";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import getPosts from "./api/posts/posts";
 import { TPost } from "./types/index.ts";
 import PostCard from "./components/postCard";
+import PostModal from "./components/postModal";
 
 function App() {
   const [posts, setPosts] = useState<TPost[]>([]);
+  const [selectedPost, setSelectedPost] = useState<TPost | null>(null);
+  const modalRef = useRef<HTMLDialogElement>(null);
   useEffect(() => {
     getPosts().then((res) => setPosts(res));
   }, []);
 
+  const toggleModal = () => {
+    if (!modalRef.current) {
+      return;
+    }
+
+    modalRef.current?.hasAttribute("open")
+      ? modalRef.current.close()
+      : modalRef.current.showModal();
+  };
+
   return (
-    <main>
-      <div className="posts-container">
-        {posts.map((post) => (
-          <PostCard
-            post={post}
-            key={post.id}
-          />
-        ))}
-      </div>
-    </main>
+    <>
+      {!!selectedPost && (
+        <PostModal
+          post={selectedPost}
+          toggleModal={toggleModal}
+          ref={modalRef}
+        />
+      )}
+      <main>
+        <div className="posts-container">
+          {posts.map((post) => (
+            <PostCard
+              post={post}
+              key={post.id}
+              setSelectedPost={setSelectedPost}
+              toggleModal={toggleModal}
+            />
+          ))}
+        </div>
+      </main>
+    </>
   );
 }
 
